bfd197a4e7ee66519dd0ae78a0e6a9517c57ad11
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-05 19:47:57 +0200

    move mongodb-init.ts to infrastructure dir

--- src/infrastructure/mongodb-init.ts
+++ src/infrastructure/mongodb-init.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-export default class MongoDbInit{
-    static connect(){
+export default class MongoDb{
+    static init(){
         mongoose.connect('mongodb://localhost:27017/fatura', err => {
              if(err){
                  throw new Error('[*] Database Error  while Connection ${err}');
