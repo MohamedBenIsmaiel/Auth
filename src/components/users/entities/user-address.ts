b4affcf1abdf9aeee1dd65b73994ded4993050ed
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-06 00:20:08 +0200

    create user-address, the value object of user entity

---
+++ src/components/users/entities/user-address.ts
@@ -0,0 +1,22 @@
+import { IAddress } from '../type';
+import Entity from '../../../core';
+
+export default function buildAddress() {
+  return class UserAddress extends Entity {
+    private country: string;
+    private city: string;
+    private street: string;
+
+    constructor(addressPayload: IAddress) {
+      super();
+
+      this.country = addressPayload.country;
+      this.city = addressPayload.city;
+      this.street = addressPayload.street;
+
+      this.validate();
+    }
+
+    validate(){};
+  };
+}
