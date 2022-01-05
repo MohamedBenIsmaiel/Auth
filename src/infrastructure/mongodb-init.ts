735253933b44fa0cc299dfab77e68db558a10555
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-05 22:00:36 +0200

    import config vars from config

--- src/infrastructure/mongodb-init.ts
+++ src/infrastructure/mongodb-init.ts
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
+import { dbName, dbHost, dbPort } from "../config";
 
 export default class MongoDb{
+
+    static dbName = dbName;
+    static dbPort = dbPort;
+    static dbHost = dbHost;
+    static uri    = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+
     static init(){
-        mongoose.connect('mongodb://localhost:27017/fatura', err => {
+        mongoose.connect(MongoDb.uri, err => {
              if(err){
                  throw new Error('[*] Database Error  while Connection ${err}');
              }
