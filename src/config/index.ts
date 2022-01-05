244d44cc2add66ef215bd6b33b35364586f0af08
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-05 22:20:31 +0200

    formatting the code like replace double quotes with single quotes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
-import * as dotenv from "dotenv";
-import path from "path";
+import * as dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({path: path.join(__dirname, "../../.env")});
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 export const serverPort = process.env.SERVER_PORT;
 export const dbHost = process.env.DB_HOST;
--- src/infrastructure/mongodb-init.ts
+++ src/infrastructure/mongodb-init.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import { dbName, dbHost, dbPort } from "../config";
+import mongoose from 'mongoose';
+import { dbName, dbHost, dbPort } from '../config';
 
-export default class MongoDb{
+export default class MongoDb {
+  static dbName = dbName;
+  static dbPort = dbPort;
+  static dbHost = dbHost;
+  static uri = `mongodb://${dbHost}:${dbPort}/${dbName}`;
 
-    static dbName = dbName;
-    static dbPort = dbPort;
-    static dbHost = dbHost;
-    static uri    = `mongodb://${dbHost}:${dbPort}/${dbName}`;
-
-    static init(){
-        mongoose.connect(MongoDb.uri, err => {
-             if(err){
-                 throw new Error('[*] Database Error  while Connection ${err}');
-             }
-             console.log('[*] Database Connected sucessfuly')
-        });
-    }
+  static init() {
+    mongoose.connect(MongoDb.uri, (err) => {
+      if (err) {
+        throw new Error('[*] Database Error  while Connection ${err}');
+      }
+      console.log('[*] Database Connected sucessfuly');
+    });
+  }
 }
