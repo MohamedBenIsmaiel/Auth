6cb9dcd8046dd46d9c40add496c2bcbf2ed536a7
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-06 00:19:15 +0200

    create user entity that include the base of the business of our app

---
+++ src/components/users/entities/user.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { IUser, IBuildUser } from '../type';
+import Entity from '../../../core';
+
+export default function buildUser({ userEnums, UserAddress }: IBuildUser) {
+  return class User extends Entity {
+    private id?: string;
+    private name: string;
+    private email: string;
+    private address: typeof UserAddress | {};
+    private mobileNumber: string;
+    private role: string;
+    private hobbies?: string[];
+
+    constructor(userPayload: IUser) {
+      super();
+
+      this.id = userPayload.id;
+      this.name = userPayload.name;
+      this.email = userPayload.email;
+      this.address = new UserAddress(userPayload.address || {}).toJson();
+      this.mobileNumber = userPayload.mobileNumber;
+      this.role = userPayload.role;
+      this.hobbies = userPayload.hobbies;
+
+      this.validate();
+    }
+
+    validate(){};
+  };
+}
