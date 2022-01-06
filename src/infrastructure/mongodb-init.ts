d9f2a8cd791fb0c04ddce8bb4324e9749694b8e7
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-06 14:13:40 +0200

    import new vars from config

--- src/infrastructure/mongodb-init.ts
+++ src/infrastructure/mongodb-init.ts
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
-import { dbName, dbHost, dbPort } from '../config';
+import {
+  dbName,
+  dbHost,
+  dbPort,
+  dbUsername,
+  dbPassword,
+  dbAuthSource
+} from '../config';
 
 export default class MongoDb {
   static dbName = dbName;
   static dbPort = dbPort;
   static dbHost = dbHost;
-  static uri = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+  static dbUsername = dbUsername;
+  static dbPassword = dbPassword;
+  static dbAutSource = dbAuthSource;
+  static uri = `mongodb://ismaiel:fk_system();@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
 
   static init() {
     mongoose.connect(MongoDb.uri, (err) => {
