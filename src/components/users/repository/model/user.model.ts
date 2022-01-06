4768dfce1409f6a2a3dcd599f1f67df9dfbd87c9
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-06 18:21:30 +0200

    update model by adding third param o model

--- src/components/users/repository/model/user.model.ts
+++ src/components/users/repository/model/user.model.ts
@@ -3,18 +3,28 @@ import { Schema, model } from 'mongoose';
 import { IUser } from '../../type';
 import { userEnums } from '../../entities';
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  address: {
-    country: { type: String },
-    city: { type: String },
-    street: { type: String }
+const user = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    address: {
+      country: { type: String },
+      city: { type: String },
+      street: { type: String }
+    },
+    mobileNumber: { type: String },
+    role: { type: String, enum: userEnums.values() },
+    hobbies: [{ type: String }]
   },
-  mobileNumber: { type: String },
-  role: { type: String, enum: userEnums.values() },
-  hobbies: [{ type: String }]
+  { timestamps: true, minimize: false }
+);
+
+user.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform(doc, ret) {
+    delete ret._id;
+  }
 });
 
-const UserModel = model<IUser>('User', userSchema);
-export default UserModel;
+export default model<IUser>('User', user, 'users');
