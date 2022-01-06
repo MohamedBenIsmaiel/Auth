8b46179bed3a3cea12839d178b2c4e609d70e7ed
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-06 18:23:23 +0200

    update mongodb-init by listening on some events ike SIGNIT

--- src/infrastructure/mongodb-init.ts
+++ src/infrastructure/mongodb-init.ts
@@ -15,14 +15,29 @@ export default class MongoDb {
   static dbUsername = dbUsername;
   static dbPassword = dbPassword;
   static dbAutSource = dbAuthSource;
-  static uri = `mongodb://ismaiel:fk_system();@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
+  static uri = `mongodb://${dbUsername}:${dbPassword}@${dbHost}:${dbPort}/${dbName}?authsource=${dbAuthSource}`;
 
   static init() {
-    mongoose.connect(MongoDb.uri, (err) => {
-      if (err) {
-        throw new Error('[*] Database Error  while Connection ${err}');
-      }
-      console.log('[*] Database Connected sucessfuly');
+    mongoose.connect(MongoDb.uri).catch((e) => {
+      console.log('[*] Database Error  ', e);
+      process.exit(1);
+    });
+
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose connected to db');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.log(err.message);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('Mongoose connection is disconnected.');
+    });
+
+    process.on('SIGINT', async () => {
+      await mongoose.connection.close();
+      process.exit(0);
     });
   }
 }
