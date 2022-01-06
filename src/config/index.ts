a50eb69ecd749ff1ff303d0ed662763c9a7ebe1a
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-06 14:14:00 +0200

    add new vars to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,3 +7,6 @@ export const serverPort = process.env.SERVER_PORT;
 export const dbHost = process.env.DB_HOST;
 export const dbPort = process.env.DB_PORT;
 export const dbName = process.env.DB_NAME;
+export const dbUsername = process.env.DB_USERNAME;
+export const dbPassword = process.env.DB_PASSWORD;
+export const dbAuthSource = process.env.DB_AUTH_SOURCE;
