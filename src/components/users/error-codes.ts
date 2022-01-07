e56de486c94bb6d88f253f46a9dc3306728bd1ac
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-08 00:31:16 +0200

    create ErrorCodes class that includes Error codes like notfound , unauthenticated and etc ..

--- src/components/users/error-codes.ts
+++
@@ -1,4 +0,0 @@
-export class ErrorCodes {
-  public static readonly Unauthenticated = 'Unauthenticated';
-  public static readonly NotFound = 'NotFound';
-}
---
+++ src/errors-handler/error-codes.ts
@@ -0,0 +1,6 @@
+export default class ErrorCodes {
+  public static readonly Unauthenticated = 'Unauthenticated';
+  public static readonly NotFound = 'NotFound';
+  public static readonly Validation = 'Validation';
+  public static readonly UnknownError = 'UnknownError';
+}
