e9f86f5cd8d689b3d656accddea22aff37744d0d
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-07 23:20:12 +0200

    delete un-need method

--- src/components/users/entities/user-address.ts
+++ src/components/users/entities/user-address.ts
@@ -3,9 +3,9 @@ import Entity from '../../../core';
 
 export default function buildAddress() {
   return class UserAddress extends Entity {
-    private country: string;
-    private city: string;
-    private street: string;
+    private country?: string;
+    private city?: string;
+    private street?: string;
 
     constructor(addressPayload: IAddress) {
       super();
@@ -13,10 +13,6 @@ export default function buildAddress() {
       this.country = addressPayload.country;
       this.city = addressPayload.city;
       this.street = addressPayload.street;
-
-      this.validate();
     }
-
-    validate(){};
   };
 }
