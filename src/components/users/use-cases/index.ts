f8e7e45409bef59bf06986879bf8241829bf8819
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-08 20:56:20 +0200

    import register use-case and build it

--- src/components/users/use-cases/index.ts
+++ src/components/users/use-cases/index.ts
@@ -1,10 +1,12 @@
 import buildListUsers from './list-users';
 import buildGetMyProfile from './get-my-profile';
+import buildRegister from './register';
 
 import UsersDb from '../repository';
 
 const listUsers = buildListUsers({ UsersDb });
 const getMyProfile = buildGetMyProfile({ UsersDb });
+const register = buildRegister({ UsersDb });
 
-const UserUsecase = { listUsers, getMyProfile };
+const UserUsecase = { listUsers, getMyProfile, register };
 export default UserUsecase;
