6c5889058c2fe8177000a18d4a75cd159c255b34
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-08 20:47:35 +0200

    adding register route

--- src/components/users/routes.ts
+++ src/components/users/routes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 import UserController from './controller';
 import { userPath } from './config';
+import { asyncHandler } from '../../middlewares/indext';
 
 const router: Router = express.Router();
 
 router.get(userPath.listUsers, UserController.listUser);
 router.get(userPath.viewProfile, UserController.getMyprofile);
+router.post(userPath.register, UserController.register);
 
 export default router;
