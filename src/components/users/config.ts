59af492e803016da72e10a14941b3ab89a111de6
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-08 20:47:09 +0200

    adding invalidCharacters regex && allowedDomain array

--- src/components/users/config.ts
+++ src/components/users/config.ts
@@ -1,6 +1,9 @@
 export const userPath = {
   listUsers: '/list-users',
-  viewProfile: '/view-profile'
+  viewProfile: '/view-profile',
+  register: '/register'
 };
 
 export const saltRounds = 10; // used to generate salt for hashing password
+export const invalidCharacters = new RegExp(/^.*[<>{}].*$/s);
+export const allowedDomain = ['com', 'net', 'org', 'io', 'ai'];
