edef55dcb800c092790b70f32490189bed8bf73c
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-08 21:36:23 +0200

    pass option param to toJson method to delete password

--- src/core/entity.ts
+++ src/core/entity.ts
@@ -1,6 +1,7 @@
 export default abstract class Entity {
-  toJson(): this {
-    const obj = { ...this };
+  toJson(options?: { deletePassword: boolean }): this {
+    const obj: any = { ...this };
+    if (options && options.deletePassword) delete obj.password;
     return obj;
   }
 }
