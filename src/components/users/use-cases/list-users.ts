30b4af31255ff6fb24226005cf47f728f9e4e82e
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-09 04:09:04 +0200

    update list-users use-case , only admin can access it

--- src/components/users/use-cases/list-users.ts
+++ src/components/users/use-cases/list-users.ts
@@ -1,9 +1,13 @@
 import { IBuildListUsers, TBuildListUsers } from '../type';
+import { Permission } from '../services/index';
+import { ErrorCodes, ErrorException } from '../../../errors-handler';
 
 export default function buildListUsers({
   UsersDb
 }: IBuildListUsers): TBuildListUsers {
-  return async function listUsers() {
+  return async function listUsers(user) {
+    if (!Permission.isAdmin(user.role))
+      throw new ErrorException('Permission denied ', ErrorCodes.Permission);
     return UsersDb.find({});
   };
 }
