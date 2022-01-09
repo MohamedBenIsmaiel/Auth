8b3c797f01db852b29d6bed39537328c5000bf2b
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-09 03:12:41 +0200

    add login to userPath and add issuer

--- src/components/users/config.ts
+++ src/components/users/config.ts
@@ -1,9 +1,11 @@
 export const userPath = {
   listUsers: '/list-users',
   viewProfile: '/view-profile',
-  register: '/register'
+  register: '/register',
+  login: '/login'
 };
 
 export const saltRounds = 10; // used to generate salt for hashing password
 export const invalidCharacters = new RegExp(/^.*[<>{}].*$/s);
 export const allowedDomain = ['com', 'net', 'org', 'io', 'ai'];
+export const issuer = 'Faturab2b.com';
