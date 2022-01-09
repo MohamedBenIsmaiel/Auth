99b6659bca9ba68af7ea943e23a57d67eb9c68b5
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-09 03:05:26 +0200

    import buildLogin and  export login as a user use-case

--- src/components/users/use-cases/index.ts
+++ src/components/users/use-cases/index.ts
@@ -1,12 +1,14 @@
 import buildListUsers from './list-users';
 import buildGetMyProfile from './get-my-profile';
 import buildRegister from './register';
+import buildLogin from './login';
 
 import UsersDb from '../repository';
 
 const listUsers = buildListUsers({ UsersDb });
 const getMyProfile = buildGetMyProfile({ UsersDb });
 const register = buildRegister({ UsersDb });
+const login = buildLogin({ UsersDb });
 
-const UserUsecase = { listUsers, getMyProfile, register };
+const UserUsecase = { listUsers, getMyProfile, register, login };
 export default UserUsecase;
