8de8c31704be9f571ac9dc51ab1953b02428b2c2
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-09 14:19:52 +0200

    init refresh-token use-case

--- src/components/users/use-cases/index.ts
+++ src/components/users/use-cases/index.ts
@@ -2,6 +2,7 @@ import buildListUsers from './list-users';
 import buildGetMyProfile from './get-my-profile';
 import buildRegister from './register';
 import buildLogin from './login';
+import buildRefreshToken from './refresh-token';
 
 import UsersDb from '../repository';
 
@@ -9,6 +10,7 @@ const listUsers = buildListUsers({ UsersDb });
 const getMyProfile = buildGetMyProfile({ UsersDb });
 const register = buildRegister({ UsersDb });
 const login = buildLogin({ UsersDb });
+const refreshToken = buildRefreshToken();
 
-const UserUsecase = { listUsers, getMyProfile, register, login };
+const UserUsecase = { listUsers, getMyProfile, register, login, refreshToken };
 export default UserUsecase;
---
+++ src/components/users/use-cases/refresh-token.ts
@@ -0,0 +1,21 @@
+import { ErrorCodes, ErrorException } from '../../../errors-handler';
+import { Token } from '../services';
+import { ILogin } from '../type';
+
+export default function buildRefreshToken(): ({
+  refToken
+}: any) => Promise<ILogin> {
+  return async function refreshTokenSecret({ refToken }): Promise<ILogin> {
+    if (!refToken)
+      throw new ErrorException(
+        'Please provide a refresh token ',
+        ErrorCodes.Unauthenticated
+      );
+
+    const { userId, role }: any = await Token.verifyRefreshToken(refToken);
+    console.log('verify refresh token');
+    const accessToken = await Token.signeAccessToken({ userId, role });
+    const refreshToken = await Token.signeRefreshToken({ userId, role });
+    return { accessToken, refreshToken };
+  };
+}
