ead72ec73e389b9cc0eb0e7691c152e7c750d395
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-01-09 03:09:50 +0200

    add login method to user controller

--- src/components/users/controller/user.controller.ts
+++ src/components/users/controller/user.controller.ts
@@ -6,19 +6,24 @@ export default function buildUserController({
   UserUsecase
 }: IBuildUserController) {
   return class UserController {
-    static async listUser(_req: Request, res: Response): Promise<Response> {
+    static async listUser(req: Request, res: Response): Promise<Response> {
       const users = await UserUsecase.listUsers();
       return res.json({ statusCode: 200, data: users });
     }
 
-    static async getMyprofile(_req: Request, res: Response): Promise<Response> {
-      const myProfile = await UserUsecase.getMyProfile();
+    static async getMyprofile(req: any, res: Response): Promise<Response> {
+      const myProfile = await UserUsecase.getMyProfile(req.user);
       return res.json({ statusCode: 200, data: myProfile });
     }
 
     static async register(req: Request, res: Response): Promise<Response> {
       const registeredUser = await UserUsecase.register(req.body);
       return res.json({ statusCode: 200, data: registeredUser });
+    }
+
+    static async login(req: Request, res: Response): Promise<Response> {
+      const tokens = await UserUsecase.login(req.body);
+      return res.json({ statusCode: 200, data: tokens });
     }
   };
 }
